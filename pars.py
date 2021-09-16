d4e5e1ab1820c81b7240a7280cc096ef0de0918d
Aidar <gluhovkich@gmail.com>
2021-09-16 21:24:31 +0300

    Everything that has been parsed is added to db.

--- pars.py
+++ pars.py
@@ -2,8 +2,11 @@
 
 import requests
 from bs4 import BeautifulSoup
-import json
 from fake_useragent import UserAgent
+import psycopg2
+
+connection = psycopg2.connect('''connection to db''')
+cursor = connection.cursor()
 
 
 
@@ -17,30 +20,30 @@ for i in range(33):
     # Парсинг всех цитат с девяти страниц.
     url_page = url + '/page/' + str(i)
     response = requests.get(url_page, headers=header)
-    print(response.status_code)
+
     bs = BeautifulSoup(response.text, 'lxml')
 
+    # Нахожу все цитаты с сайта
     quotes = bs.find('div', {'id': 'content'}).find_all('div', class_='quote')
-    
+
     for info_of_quote in quotes:
+        # Проходится по списку всех цитат, избавляет от тегов и добавляет по переменным
         quote = info_of_quote.find('div', class_='quote-text').find('p').text
         author_and_book_name = info_of_quote.find('p', class_='source').find_all('a')
         try:
             author = author_and_book_name[0].text
             book_name = author_and_book_name[1].text
-            quotes_author_and_book_name.append(
-                {
-                    'Автор': author,
-                    'Название произведения': book_name,
-                    'Цитата': quote
-                }
-            )
+            with connection.cursor() as cursor:
+                # Добавляет цитаты в бд
+                cursor.execute("""INSERT INTO quobook(id, author, "book name", quote) VALUES (%s, %s, %s, %s)""",
+                               (count, author, book_name, quote))
+                connection.commit()
+
         except Exception as e:
+            connection.rollback()
             print(repr(e))
+
         print(count)
         count += 1
-
-with open('quotes.json', 'a', encoding='utf-8') as file:
-    json.dump(quotes_author_and_book_name, file, indent=4, ensure_ascii=False)
 
 print('Всё готово!')
