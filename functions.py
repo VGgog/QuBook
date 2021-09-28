9e7a9debfa0f3375409c4636a65f4a534e40ca05
Aidar <gluhovkich@gmail.com>
2021-09-28 19:18:30 +0300

    Add a check_correct_data function

--- functions.py
+++ functions.py
@@ -8,22 +8,22 @@ db = DateBaseA()
 
 def give_a_nice_quote(info_for_quote):
     """Возвращает цитату в виде списка"""
-    if isinstance(info_for_quote[0], list) or isinstance(info_for_quote[0], tuple):
-        info_for_quote = info_for_quote[0] 
-        return {'ID': info_for_quote[0],
-                'Author': info_for_quote[1],
-                'Book title': info_for_quote[2],
-                'Quote': info_for_quote[3]
-                }
-    elif isinstance(info_for_quote, dict):
-        return {'ID': info_for_quote['ID'],
-                'Author': info_for_quote['Author'],
-                'Book title': info_for_quote['Book title'],
-                'Quote': info_for_quote['Quote']
-                }
+    if info_for_quote:
+        if isinstance(info_for_quote, list) or isinstance(info_for_quote, tuple):
+            info_for_quote = info_for_quote[0]
+            return {'ID': info_for_quote[0],
+                    'Author': info_for_quote[1],
+                    'Book title': info_for_quote[2],
+                    'Quote': info_for_quote[3]
+                    }
+        elif isinstance(info_for_quote, dict):
+            return {'ID': info_for_quote['ID'],
+                    'Author': info_for_quote['Author'],
+                    'Book title': info_for_quote['Book title'],
+                    'Quote': info_for_quote['Quote']
+                    }
     else:
-        return "Error"
-
+        return "Error, this quote not found"
 
 
 def return_list_result(checklist):
@@ -43,4 +43,14 @@ def return_sorted_quotes(count, sorting, index):
                 sorted_quotes.append(give_a_nice_quote(information_for_quote))
 
     return sorted_quotes
+
+
+def check_correct_data(data):
+    """"""
+    if 'Author' in data:
+        if 'Book title' in data:
+            if 'Quote' in data:
+                return True
+
+    return False
 
